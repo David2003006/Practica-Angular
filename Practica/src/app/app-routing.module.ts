7e39874946e5759ed34cd73aaa24b64ffa756720
David2003006 <davidalejandro2003@yahoo.com>
2025-01-02 12:37:30 -0600

    Es la ruta de mi aplicacion principal

--- Practica/src/app/app-routing.module.ts
+++ Practica/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'personal', loadChildren:() => import('./Modules/registros/registros.module').then(m => m.RegistrosModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
